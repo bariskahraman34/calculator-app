afdcd76529561a8444345fb7872f2038aa1ddc5f
Barış Kahraman <baris-kahra@hotmail.com>
2024-01-26 03:18:45 +0300

    Hesap makinesi işlemi tamamlandı. fonksiyonlar çalışıyor. Ekstra işlemler yapılacak

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,7 @@
 const numbers = document.querySelectorAll('.number');
 const operators = document.querySelectorAll('.operator');
-const result = document.querySelector('.result');
+const currentResult = document.querySelector('#current-result');
+const previousResult = document.querySelector('#previous-result');
 const del = document.querySelector('#del');
 const reset = document.querySelector('#reset');
 const equal = document.querySelector('#equal');
@@ -8,3 +9,38 @@ const addition = document.querySelector('#addition');
 const subtraction = document.querySelector('#subtraction');
 const multiply = document.querySelector('#multiply');
 const division = document.querySelector('#division');
+
+for (const number of numbers) {
+    number.addEventListener('click' , (e) => printNumberToScreen(e));
+}
+
+for (const operator of operators) {
+    operator.addEventListener('click' , (e) => printOperatorToScreen(e));
+}
+
+reset.addEventListener('click' , clear);
+
+equal.addEventListener('click', calculate);
+
+del.addEventListener('click', removeLastNumber);
+
+function printNumberToScreen(e){
+    currentResult.innerHTML += e.target.innerHTML;
+}
+
+function printOperatorToScreen(e){
+    currentResult.innerHTML += e.target.innerHTML;
+}
+
+function clear(){
+    currentResult.innerHTML = "";
+    previousResult.innerHTML = "";
+}
+
+function calculate(){
+    currentResult.innerHTML = eval(currentResult.innerHTML);
+}
+
+function removeLastNumber(){
+    currentResult.innerHTML = currentResult.innerHTML.split('').slice(0 , -1).join('');
+}
